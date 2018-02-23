a9e756e3bff3124abfbd1caf8f0553f53801b1ca
= <=>
2018-02-23 15:46:54 -0500

    'msg'

--- Dev/scripts/main.js
+++ Dev/scripts/main.js
@@ -7,3 +7,16 @@
 // let total_length = path.getTotalLength();
 
 // })(window, document);
+
+
+      discordWidget.init({
+          serverId: '364473157312315394',
+          title: 'Devvit.io',
+          join: true,
+          alphabetical: true,
+          theme: 'dark',
+          hideChannels: true,
+          showAllUsers: true,
+          allUsersDefaultState: false
+      });
+      discordWidget.render();
